8aa392bcf9f4deda2ee065bfa454458eb76a979a
AmrTarek9595 <amr.ratebyan@gmail.com>
2024-05-20 15:03:39 +0200

    edit last

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@
   \*********************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict"
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   createPopper: () => (/* binding */ createPopper),
