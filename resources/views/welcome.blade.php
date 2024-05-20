9f493f6234a4d23502a1315d6ee05392dd29ea27
AmrTarek9595 <amr.ratebyan@gmail.com>
2024-05-20 14:07:45 +0200

    update index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <title>Laravel</title>
 
       
@@ -12,6 +12,11 @@
         <div id="app">
             <layout></layout>
 </div>
-<script src="{{asset('js/app.js')}}"></script>
-    </body>
+        <script src="{{asset('js/app.js')}}"></script>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" ></script>
+
+</body>
 </html>
